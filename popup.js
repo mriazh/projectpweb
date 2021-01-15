b17543e132592c023b0764782d83be20412e0648
Oscar Daniel <71881424+Kurizen@users.noreply.github.com>
2021-01-15 10:40:27 +0700

    DaPur

---
+++ popup.js
@@ -0,0 +1,8 @@
+function pop() {
+    alert("Email restorasi kata sandi telah dikirim, silahkan cek email Anda.");
+}
+
+function testpopup() {
+    var popup = document.getElementById("testpop");
+    popup.classList.toggle("show")
+}
